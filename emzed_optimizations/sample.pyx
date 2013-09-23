45c8173690a6923868b1439f4f7cf36f75666227
Uwe Schmitt <uschmitt@mineway.de>
2013-09-23 17:26:11 +0200

    [FEATURE] added sample_peaks

--- emzed_optimizations/sample.pyx
+++ emzed_optimizations/sample.pyx
@@ -2,7 +2,8 @@ cimport cython
 cimport numpy as np
 import numpy as np
 
-from libc.stdlib cimport malloc, free
+from libc.stdlib cimport malloc, free, calloc
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -67,7 +68,74 @@ def chromatogram(pm, double mzmin, double mzmax, double rtmin, double rtmax, int
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def sample_image(pm, float rtmin, float rtmax, float mzmin, float mzmax, int w, int h):
+def sample_peaks(pm, float rtmin, float rtmax, float mzmin, float mzmax, size_t n_bins):
+
+    cdef double * i_sums
+    cdef double * mz_i_sums
+    cdef double mz, ii
+
+    cdef list spectra
+    cdef np.float32_t[:, :] peaks
+
+    cdef size_t ns, n, i, j, mz_bin
+    cdef double rt
+
+    cdef np.ndarray result
+
+    i_sums = <double * > calloc(sizeof(double), n_bins)
+    if i_sums == NULL:
+        return None
+    mz_i_sums = <double * > calloc(sizeof(double), n_bins)
+    if mz_i_sums == NULL:
+        free(i_sums)
+        return None
+
+    spectra = pm.spectra
+    ns = len(spectra)
+
+    # I tried splitting the loops, so that a first loop runs i up to rtmin, and a second loop
+    # which runs to rtmax. I proceeded like this for the inner loop from mzmin to mzmax,
+    # but this did not result in a performance gain, but the code got more complex, so I
+    # just use for-loops below with full range testing in the loop bodies
+
+    for i in range(ns):
+        spec = spectra[i]
+        rt = spec.rt
+        if rt < rtmin:
+            continue
+        if rt > rtmax:
+            break
+        if spec.msLevel != 1:
+            continue
+        peaks = spec.peaks
+        n = peaks.shape[0]
+        for j in range(n):
+            mz = peaks[j, 0]
+            if mzmin <= mz and mz <= mzmax:
+                mz_bin = int((mz - mzmin) / (mzmax - mzmin) * (n_bins - 1))
+                ii = peaks[j, 1]
+                i_sums[mz_bin] += ii
+                mz_i_sums[mz_bin] += mz * ii
+
+    result = np.zeros((n_bins, 2), dtype=np.float32)
+    peaks = result  # create view
+    i = 0
+    j = 0
+    for i in range(n_bins):
+        ii = i_sums[i]
+        if ii > 0:
+            peaks[j, 0] = mz_i_sums[i] / ii
+            peaks[j, 1] = ii
+            j += 1
+
+    free(i_sums)
+    free(mz_i_sums)
+    return result[:j, :]
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def sample_image(pm, float rtmin, float rtmax, float mzmin, float mzmax, size_t w, size_t h):
 
     cdef np.ndarray img = np.zeros((h, w), dtype=np.float64)
     cdef np.float64_t[:, :] img_view = img
@@ -81,7 +149,7 @@ def sample_image(pm, float rtmin, float rtmax, float mzmin, float mzmax, int w, 
 
     # I tried splitting the loops, so that a first loop runs i up to rtmin, and a second loop
     # which runs to rtmax. I proceeded like this for the inner loop from mzmin to mzmax,
-    # but this did not result in a performance gain, but the code got more comples, so I
+    # but this did not result in a performance gain, but the code got more complex, so I
     # just use for-loops below with full range testing in the loop bodies
 
     for i in range(ns):
